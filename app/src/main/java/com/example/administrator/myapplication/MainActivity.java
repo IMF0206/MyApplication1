7e0017a9efb76dcb3d5792cd1dc7b49b0c10a81d
pengjialing <15298392120@163.com>
2017-10-12 23:35:36 +0800

    add simple new class

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.administrator.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.administrator.myapplication.textutils.TextUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextUtil util = new TextUtil();
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText(util.getvalue());
     }
 }
 
